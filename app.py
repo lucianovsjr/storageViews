ee2986fe3692bab99989c758f23da58d9234d0dd
Luciano Vieira <lucianovsjr@gmail.com>
2020-09-15 14:09:04 -0300

    Sync users and roles

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import pymssql
 
 from datetime import datetime
 from pymongo import MongoClient
-from decouple import config
+from decouple import config, Csv
 
+
+DISCARD_VIEWS = config('DISCARD_VIEWS', cast=Csv())
+DROP_USER = config('DROP_USER', cast=bool)
 
 logging.basicConfig(filename='app.log', level=logging.INFO)
 logging.info('Exec Inicio {}'.format(datetime.now()))
@@ -30,19 +33,25 @@ try:
     mssql_cursor.execute('SELECT DISTINCT(ObjectName) FROM VPERMVIEWS')
 except Exception as e:
     print(e)
-
+# Sincronizar Views
 for row in mssql_cursor:
     view_name = row.get('ObjectName')
 
-    sqlite_cursor.execute(f'SELECT name FROM views WHERE name = "{view_name}"')
-    if not sqlite_cursor.fetchone():
+    sqlite_cursor.execute(f'SELECT name, enabled FROM views WHERE name = "{view_name}"')
+    sqlite_row = sqlite_cursor.fetchone()
+
+    if not sqlite_row:
         sqlite_cursor.execute(f'INSERT INTO views VALUES ("{view_name}", 1)')
+        logging.info(f'New View {view_name}')
+
+    if view_name in DISCARD_VIEWS and sqlite_row[1] == 1:
+        sqlite_cursor.execute(f'UPDATE views SET enabled = 0 WHERE name = "{view_name}"')
+        logging.info(f'Discard View {view_name}')
 
 views = []
 sqlite_cursor.execute('SELECT name FROM views WHERE enabled = 1')
 for row in sqlite_cursor:
     views.append(row[0])
-views = []
 
 sqlite_conn.commit()
 sqlite_conn.close()
@@ -50,14 +59,58 @@ sqlite_conn.close()
 mongo_client = MongoClient(config('MONGO_HOST'))
 mongo_db = mongo_client.views
 
+# Sincronizar usários
+try:
+    mssql_cursor.execute('''
+        SELECT
+            SUBSTRING(DatabaseUserName, 15, len(DatabaseUserName) - 13) AS DatabaseUserName,
+            Stuff(
+                (
+                    Select ',' + Cast(ObjectName As varchar(100)) From VPERMVIEWS VIEWS
+                    Where VIEWS.DatabaseUserName = VPERMVIEWS.DatabaseUserName
+                        For Xml Path('')
+                ), 1, 1, ''
+            ) AS ObjectName
+        FROM VPERMVIEWS
+        GROUP BY DatabaseUserName
+        ORDER BY DatabaseUserName
+    ''')
+except Exception as e:
+    print(e)
+for row in mssql_cursor:
+    db_user = row.get('DatabaseUserName')
+    db_pass = '123456'
+    views_name = row.get('ObjectName')
+
+    roles_info = mongo_db.command('rolesInfo', db_user)
+    mongo_db.command(
+        'updateRole' if roles_info['roles'] else 'createRole',
+        db_user,
+        privileges = [{
+            'actions': ['find'],
+            'resource': {'db': 'views', 'collection': view_name}
+        } for view_name in views_name.split(',')],
+        roles = []
+    )
+
+    users_info = mongo_db.command('usersInfo', db_user)
+    if not users_info['users']:
+        logging.info(f'New User {db_user} {db_pass}')
+        mongo_db.command('createUser', db_user, pwd=db_pass, roles=[db_user])
+
+    if DROP_USER:
+        mongo_db.command('dropRole', db_user)
+        mongo_db.command('dropUser', db_user)
+
 for view in views:
     mongo_collection = mongo_db[view]
 
     time_start = time.time()
     try:
-        mssql_cursor.execute(f'SELECT * FROM {view}')
+        mssql_cursor.execute(f'DELETE FROM {view}')
+        mssql_cursor.execute(f'SELECT TOP 10 * FROM {view}')
     except Exception as e:
-        logging.info('{} Erro {}'.format(view, e)
+        logging.info('{} Erro {}'.format(view, e))
 
     for row in mssql_cursor:
         row_validated = dict()
